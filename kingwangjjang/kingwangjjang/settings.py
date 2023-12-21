e751269c63a75c09fca1c8370fc1fe2b0bf51dfd
Hwanju <hwanju1596@gmail.com>
2023-12-21 11:12:56 +0900

    :closed_lock_with_key: SECRET_KEY 분리

--- kingwangjjang/kingwangjjang/settings.py
+++ kingwangjjang/kingwangjjang/settings.py
@@ -20,7 +20,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_je$h)$q827=7f2^e$ogm$44gn##c34j15jx^vi7fjof9d_evx'
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') 
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
