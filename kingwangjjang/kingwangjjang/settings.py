1f944e560dbccd02cf1b7980a05d126ed9ccad5e
Hwanju <hwanju1596@gmail.com>
2024-01-15 10:19:25 +0900

    Setup: 보안상의 이유로 Secrets.json을 지우고 githubactioni repository secret으로 변경

--- kingwangjjang/kingwangjjang/settings.py
+++ kingwangjjang/kingwangjjang/settings.py
@@ -23,19 +23,31 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os, json
 from django.core.exceptions import ImproperlyConfigured
 
-secret_file = os.path.join(BASE_DIR, '~/secrets.json') 
+## Local settings
+# secret_file = os.path.join(BASE_DIR, '~/secrets.json') 
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
 
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+# def get_secret(setting, secrets=secrets):
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = "Set the {} environment variable".format(setting)
+#         raise ImproperlyConfigured(error_msg)
+#
+# DB_HOST = get_secret("DB_HOST")
+# DB_USER = get_secret("DB_USER")
+# DB_PASSWORD = get_secret("DB_PASSWORD")
+# DB_NAME = get_secret("DB_NAME")
 
-SECRET_KEY = get_secret("SECRET_KEY")
+# github action setting
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+DB_HOST = os.environ.get('DB_HOST')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,10 +98,7 @@ WSGI_APPLICATION = 'kingwangjjang.wsgi.application'
 
 # Database
 # MongoDB settings
-DB_HOST = get_secret("DB_HOST")
-DB_USER = get_secret("DB_USER")
-DB_PASSWORD = get_secret("DB_PASSWORD")
-DB_NAME = get_secret("DB_NAME")
+
 
 DATABASES = {
         'default': {
