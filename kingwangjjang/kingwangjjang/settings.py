71bba5ad04ad4c82d3981b47adf1ba2c7fd6b79c
Hwanju <hwanju1596@gmail.com>
2024-02-20 11:02:48 +0900

    :rocket: 배포용 변수 작업

--- kingwangjjang/kingwangjjang/settings.py
+++ kingwangjjang/kingwangjjang/settings.py
@@ -23,40 +23,40 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os, json
 from django.core.exceptions import ImproperlyConfigured
 
-# Local settings
-secret_file = os.path.join(BASE_DIR, './secrets.json') 
+# # Local settings
+# secret_file = os.path.join(BASE_DIR, './secrets.json') 
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
 
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+# def get_secret(setting, secrets=secrets):
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = "Set the {} environment variable".format(setting)
+#         raise ImproperlyConfigured(error_msg)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-SECRET_KEY = get_secret('SECRET_KEY')
-DB_HOST = get_secret("DB_HOST")
-WAS_HOST = get_secret("WAS_HOST")
-DB_USER = get_secret("DB_USER")
-DB_PASSWORD = get_secret("DB_PASSWORD")
-DB_NAME = get_secret("DB_NAME")
-FTP_USER = get_secret("FTP_USER")
-FTP_PASSWORD = get_secret("FTP_PASSWORD")
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+# SECRET_KEY = get_secret('SECRET_KEY')
+# DB_HOST = get_secret("DB_HOST")
+# WAS_HOST = get_secret("WAS_HOST")
+# DB_USER = get_secret("DB_USER")
+# DB_PASSWORD = get_secret("DB_PASSWORD")
+# DB_NAME = get_secret("DB_NAME")
+# FTP_USER = get_secret("WAS_USERNAME")
+# FTP_PASSWORD = get_secret("WAS_PASSWORD")
 
-# # github action setting
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-# DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
-# DB_HOST = os.environ.get('DB_HOST')
-# WAS_HOST = os.environ.get('WAS_HOST')
-# DB_USER = os.environ.get('DB_USER')
-# DB_PASSWORD = os.environ.get('DB_PASSWORD')
-# DB_NAME = os.environ.get('DB_NAME')
-# FTP_USER = os.environ.get('FTP_USER')
-# FTP_PASSWORD = os.environ.get('FTP_PASSWORD')
+# github action setting
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+DB_HOST = os.environ.get('DB_HOST')
+WAS_HOST = os.environ.get('WAS_HOST')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
+FTP_USER = os.environ.get('WAS_USERNAME')
+FTP_PASSWORD = os.environ.get('WAS_PASSWORD')
 
 
 ALLOWED_HOSTS = []
