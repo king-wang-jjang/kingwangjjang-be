5fe52075936dae41b5062de9a79e7a5418690f4f
hellojunho <jo62698476@gmail.com>
2024-02-29 00:50:55 +0900

    ✅ API_KEY 위치 변경 및 view 이름 변경

--- kingwangjjang/chatGPT/views.py
+++ kingwangjjang/chatGPT/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render 
 from django.http import JsonResponse 
-from .api_response import get_completion
+from .gpt_response import ChatGptApi # gpt_response.py 파일에서 클래스를 가져옵니다.
 import openai
 import os
-from kingwangjjang.settings import CHATGPT_API_KEY
+from kingwangjjang.settings import CHATGPT_API_KEY # settings.py에서 API KEY를 가져옵니다.
 
-
-openai.api_key =  CHATGPT_API_KEY #앞서 자신이 부여받은 API key를 넣으면 된다. 절대 외부에 공개해서는 안된다.
 
 # chatgpt에게 질문한 내용의 답변을 받아와 index.html로 전달합니다.
-def query_view(request): 
+def gpt_view(request): 
 	if request.method == 'POST':
 		prompt = request.POST.get('prompt') # 클라이언트의 질문 중 'prompt'인 부분을 받아옵니다.
 		prompt=str(prompt) # 문자열로 변환 후
-		response = get_completion(prompt) # gpt에게 질문합니다.
+		response = ChatGptApi.get_completion(prompt) # gpt에게 질문합니다.
 		return JsonResponse({'response': response}) # 답변을 json으로 반환합니다.
 	return render(request, 'index.html') 
