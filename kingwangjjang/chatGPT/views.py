5bf29513aa49c457e3d2cd254f53e1d785c43f10
hellojunho <jo62698476@gmail.com>
2024-02-28 20:06:55 +0900

    ♻️ gpt 질문 모듈 분리

--- kingwangjjang/chatGPT/views.py
+++ kingwangjjang/chatGPT/views.py
@@ -1,43 +1,18 @@
 from django.shortcuts import render 
 from django.http import JsonResponse 
+from .api_response import get_completion
 import openai
 import os
+from kingwangjjang.settings import CHATGPT_API_KEY
 
-openai.api_key='' #앞서 자신이 부여받은 API key를 넣으면 된다. 절대 외부에 공개해서는 안된다.
 
-messages = []
+openai.api_key =  CHATGPT_API_KEY #앞서 자신이 부여받은 API key를 넣으면 된다. 절대 외부에 공개해서는 안된다.
 
-def get_completion(content): 
-	# 사용자 입력을 받습니다.
-	# content = input("input content: ")
-	print("input message: ", content)
-
-	# 사용자 입력을 messages에 추가해줍니다.
-	messages.append({"role": "user", "content": f"{content}"})
-
-	completion = openai.ChatCompletion.create(
-		model="gpt-3.5-turbo",	# 모델 지정
-		messages=messages,		# 질문 및 답변
-		max_tokens=1024,		# 최대 토큰 수 제한
-		n=1, 					# 답변 개수
-		stop=None, 				# 답변 중단 예약어 설정
-		temperature=0.5			# 답변의 창의성 설정
-	)
-
-	# gpt로부터 응답을 받습니다.
-	# strip() : 응답이 올 때, 빈 공간 제거
-	assistance_content = completion.choices[0].message['content'].strip()
-
-	# assistance에 이전 대화 답변을 저장합니다.
-	messages.append({"role": "assistant", "content": f"{assistance_content}"})
-
-	print(f'ChatGPT : {assistance_content}')
-	return assistance_content
-
+# chatgpt에게 질문한 내용의 답변을 받아와 index.html로 전달합니다.
 def query_view(request): 
 	if request.method == 'POST':
-		prompt = request.POST.get('prompt') 
-		prompt=str(prompt)
-		response = get_completion(prompt)
-		return JsonResponse({'response': response}) 
+		prompt = request.POST.get('prompt') # 클라이언트의 질문 중 'prompt'인 부분을 받아옵니다.
+		prompt=str(prompt) # 문자열로 변환 후
+		response = get_completion(prompt) # gpt에게 질문합니다.
+		return JsonResponse({'response': response}) # 답변을 json으로 반환합니다.
 	return render(request, 'index.html') 
