c996058395e33f429924cd94d269f71ba260a012
Hwanju <hwanju1596@gmail.com>
2024-03-21 14:24:51 +0900

    :art: 전역 변수 -> self 로 수정

--- kingwangjjang/chatGPT/chatGPT.py
+++ kingwangjjang/chatGPT/chatGPT.py
@@ -1,12 +1,11 @@
 import openai
 from kingwangjjang.settings import CHATGPT_API_KEY # settings.py에서 API KEY를 가져옵니다.
 
-messages = []
 
 class ChatGPT:
-    openai.api_key = CHATGPT_API_KEY # API키 입니다. 외부에 공개하면 안됩니다.
-
     def __init__(self):
+        self.messages = []
+        openai.api_key = CHATGPT_API_KEY # API키 입니다. 외부에 공개하면 안됩니다.
         self.model = "gpt-3.5-turbo" # 모델 지정
         self.max_tokens = 1024       # 최대 토큰 수 제한
         self.n = 1                   # 답변 개수
@@ -15,7 +14,7 @@ class ChatGPT:
         
         self.completion = openai.ChatCompletion.create(
             model = self.model,
-            messages = messages,		# 질문 및 답변
+            messages = self.messages,		# 질문 및 답변
             max_tokens = self.max_tokens,
             n = self.n,
             stop = self.stop, 
@@ -23,12 +22,12 @@ class ChatGPT:
         )
 
     @classmethod
-    def get_completion(cls, content): 
+    def get_completion(self, cls, content): 
         # 사용자 입력을 받습니다.
         print("input message: ", content)
 
         # 사용자 입력을 messages에 추가해줍니다.
-        messages.append({"role": "user", "content": f"{content}"})
+        self.messages.append({"role": "user", "content": f"{content}"})
 
         completion = cls().completion
 
@@ -36,7 +35,7 @@ class ChatGPT:
         assistance_content = completion.choices[0].message['content'].strip()
 
         # assistance에 이전 대화 답변을 저장합니다.
-        messages.append({"role": "assistant", "content": f"{assistance_content}"})
+        self.messages.append({"role": "assistant", "content": f"{assistance_content}"})
 
         print(f'ChatGPT : {assistance_content}')
         return assistance_content
