bdda6e52327f43fe12b3c2c38d61c4d01f31bfa6
Hwanju <hwanju1596@gmail.com>
2024-03-25 13:21:27 +0900

    :sparkles: 이미 생성된 요약은 안하도록 함

--- kingwangjjang/webCrwaling/views.py
+++ kingwangjjang/webCrwaling/views.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from urllib import request as urllib_request
 
 from chatGPT.chatGPT import ChatGPT
+from constants import DEFAILT_GPT_ANSWER
 
 from .communityWebsite.models import RealTime
 from mongo import DBController  
@@ -17,10 +18,6 @@ from webCrwaling.communityWebsite.dcinside import Dcinside
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\nori\AppData\Local\tesseract.exe'
 
-# class RealTime(models.Model):
-
-# Create your views here.
-
 ### 이미지가 많은 상황
 # JPG -> Text 
 # 댓글을 요약 ( 추천 수가 몇 개이상 )
@@ -28,8 +25,16 @@ def CommunitySiteCrawler(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         board_id = data.get('board_id')
+        realtime_object = get_object_or_404(RealTime, _id=board_id)
+        
+        if (realtime_object.GPTAnswer != DEFAILT_GPT_ANSWER):
+            return JsonResponse({'response': realtime_object.GPTAnswer}) 
+            return True # 이미 요약이 완료된 상태
+            
+        data = json.loads(request.body)
+        board_id = data.get('board_id')
+
         dcincideCrwaller = Dcinside()
-    
         json_contents = dcincideCrwaller.get_board_contents(board_id)
         str_contents = ''
         for a in json_contents:
@@ -41,11 +46,11 @@ def CommunitySiteCrawler(request):
         chatGPT = ChatGPT()
         response = chatGPT.get_completion(content=prompt)
 
-        realtime_object = get_object_or_404(RealTime, _id=board_id)
+        # Insert Mongodb
         realtime_object.GPTAnswer = response
         realtime_object.save()
         
-    return JsonResponse({'response': response}) 
+        return JsonResponse({'response': response}) 
     
 
 def DBInsertTest():
