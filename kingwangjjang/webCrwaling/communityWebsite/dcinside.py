ea08e122ac3079f5a70589d1abda586b9c884a7c
Hwanju <hwanju1596@gmail.com>
2024-03-28 09:01:31 +0900

    :bug: selenium.common.exceptions.WebDriverException: Message: chrome not reachable

--- kingwangjjang/webCrwaling/communityWebsite/dcinside.py
+++ kingwangjjang/webCrwaling/communityWebsite/dcinside.py
@@ -142,9 +142,10 @@ class Dcinside(AbstractCommunityWebsite):
         chrome_options.add_experimental_option("prefs", prefs)
         chrome_options.add_argument('headless')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('window-size=1920x1080')
-        chrome_options.add_argument("disable-gpu")
-        chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
+        chrome_options.add_argument('--incognito')
+        chrome_options.add_argument('--disable-setuid-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_experimental_option('excludeSwitches',['enable-logging'])
 
         if not os.path.exists(self.download_path):
             os.makedirs(self.download_path)
