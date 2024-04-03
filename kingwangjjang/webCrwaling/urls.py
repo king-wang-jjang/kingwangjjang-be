2427a8d8791caa7698570819203f6bde22ce2836
Hwanju <hwanju1596@gmail.com>
2024-04-03 14:40:38 +0900

    :art: Graphql POST 기능을 위한 board_summary 분리 (RESTFul, Graphql)

--- kingwangjjang/webCrwaling/urls.py
+++ kingwangjjang/webCrwaling/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-	path('board/summary', views.board_summary, name='board_summary'), 
+	path('board/summary', views.board_summary_rest, name='board_summary'), 
     path('board/list', views.get_real_time_best, name='get_real_time_best'),
 ]
--- kingwangjjang/webCrwaling/views.py
+++ kingwangjjang/webCrwaling/views.py
@@ -18,37 +18,39 @@ from webCrwaling.communityWebsite.dcinside import Dcinside
 # JPG -> Text 
 # 댓글을 요약 ( 추천 수가 몇 개이상 )
 @csrf_exempt
-def board_summary(request):
+def board_summary(board_id):
+    realtime_object = get_object_or_404(RealTime, _id=board_id)
+     
+    if (realtime_object.GPTAnswer != DEFAILT_GPT_ANSWER):
+        return JsonResponse({'response': realtime_object.GPTAnswer}) 
+        return True # 이미 요약이 완료된 상태
+        
+    dcincideCrwaller = Dcinside()
+    json_contents = dcincideCrwaller.get_board_contents(board_id)
+    str_contents = ''
+    for a in json_contents:
+        if 'content' in a:
+            str_contents += a['content']
+    
+    # GPT 요약
+    prompt= "아래 내용에서 이상한 문자는 제외하고 5줄로 요약해줘" + str_contents
+    chatGPT = ChatGPT()
+    print("URL: https://gall.dcinside.com/board/view/?id=dcbest&no=" + board_id)
+    response = chatGPT.get_completion(content=prompt)
+
+    # Insert Mongodb
+    realtime_object.GPTAnswer = response
+    realtime_object.save()
+    
+    return JsonResponse({'response': response}) 
+
+@csrf_exempt
+def board_summary_rest(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         board_id = data.get('board_id')
-        realtime_object = get_object_or_404(RealTime, _id=board_id)
-        
-        if (realtime_object.GPTAnswer != DEFAILT_GPT_ANSWER):
-            return JsonResponse({'response': realtime_object.GPTAnswer}) 
-            return True # 이미 요약이 완료된 상태
-            
-        data = json.loads(request.body)
-        board_id = data.get('board_id')
 
-        dcincideCrwaller = Dcinside()
-        json_contents = dcincideCrwaller.get_board_contents(board_id)
-        str_contents = ''
-        for a in json_contents:
-            if 'content' in a:
-                str_contents += a['content']
-        
-        # GPT 요약
-        prompt= "아래 내용에서 이상한 문자는 제외하고 5줄로 요약해줘" + str_contents
-        chatGPT = ChatGPT()
-        print("URL: https://gall.dcinside.com/board/view/?id=dcbest&no=" + board_id)
-        response = chatGPT.get_completion(content=prompt)
-
-        # Insert Mongodb
-        realtime_object.GPTAnswer = response
-        realtime_object.save()
-        
-        return JsonResponse({'response': response}) 
+        return board_summary(board_id)
     else:
         dcincideCrwaller = Dcinside()
         dcincideCrwaller.get_real_time_best()
