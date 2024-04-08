7b7dfdb30dedbb2e05c309f67d42d51ffbe15fea
hellojunho <jo62698476@gmail.com>
2024-04-09 01:04:50 +0900

    🔨: 들여쓰기 재대로 바꿈

--- kingwangjjang/instaPost/urls.py
+++ kingwangjjang/instaPost/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
+    
 	path('post/', views.instaPost, name='instaPost'), 
     path('upload/', views.uploadPhoto, name='upload_photo'),
 	path('logout/', views.logoutView, name='logout'),
-]
+] 
