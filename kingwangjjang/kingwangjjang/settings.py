22648b171118b8eca9a71948e5c3d6c3510dccb3
Hwanju <hwanju1596@gmail.com>
2024-04-29 10:21:55 +0900

    :bug: docker Extention 수정

--- kingwangjjang/kingwangjjang/settings.py
+++ kingwangjjang/kingwangjjang/settings.py
@@ -85,9 +85,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     f'http://{WAS_HOST}:3000',
-    "http://13.125.152.57:3000"
 ]
-print(CORS_ALLOWED_ORIGINS)
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'DELETE',
