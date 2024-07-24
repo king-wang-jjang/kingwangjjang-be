e4c62fc0e02248aee6e557942502b2cd26a24b1b
황준섭 <junseopwhang@gmail.com>
2024-07-24 17:24:55 +0900

    💩 uvicorn reload 기능 활성화, dev.bash 변경

--- app/dev.bash
+++ app/dev.bash
@@ -4,4 +4,4 @@ set +a
 
 export PYTHONPATH=$(pwd)
 
-poetry run uvicorn main:app --host 0.0.0.0 --port 3000 --reload
+poetry run python main.py
--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(api.router)
 app.include_router(views.router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=3000)
+    uvicorn.run("main:app", host='0.0.0.0', port=3000,reload=True)
