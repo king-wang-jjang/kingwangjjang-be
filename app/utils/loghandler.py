e682b80515cbb68f897c2d0bae27e34041e7b4b0
황준섭 <junseopwhang@gmail.com>
2024-07-28 17:33:27 +0900

    🎨 로그 핸들러 메시지 변경

--- app/utils/loghandler.py
+++ app/utils/loghandler.py
@@ -35,7 +35,7 @@ class SlackWebhookHandler(logging.Handler):
                 "attachments": [
                     {
                         "color": color_map.get(record.levelname),
-                        "title": record.levelname,
+                        "title": f"{record.levelname}! @everyone",
                         "fields": [
                             {
                                 "title": "MESSAGE",
