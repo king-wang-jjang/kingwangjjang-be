dc0073ec67bee380a17817416a3661a847b339a5
황준섭 <junseopwhang@gmail.com>
2024-08-15 14:04:59 +0900

    :bug: 실행불가 버그수정

--- app/celery/schema.py
+++ app/celery/schema.py
@@ -1,9 +1,9 @@
-import LLM.schema
-import comment.schema
+import app.celery.LLM.schema
+import app.celery.comment.schema
 from strawberry import Schema
 from strawberry.tools import merge_types
-Query = merge_types("Query", (LLM.schema.Query, comment.schema.Query))
-status_query = merge_types("Query", (LLM.schema.TaskStatusQuery, comment.schema.TaskStatusType))
-Mutation = merge_types("Mutation", (LLM.schema.Mutation, comment.schema.Mutation))
+Query = merge_types("Query", (app.celery.LLM.schema.Query, app.celery.comment.schema.Query))
+status_query = merge_types("Query", (app.celery.LLM.schema.TaskStatusQuery, app.celery.comment.schema.TaskStatusType))
+Mutation = merge_types("Mutation", (app.celery.LLM.schema.Mutation, app.celery.comment.schema.Mutation))
 schema = Schema(query=Query, mutation=Mutation)
 task_status_schema = Schema(query=status_query)
