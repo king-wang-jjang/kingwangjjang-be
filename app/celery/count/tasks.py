2b48c7f9cd3349f79b8a3762274c42044b4cd4b6
Restyled.io <commits@restyled.io>
2024-08-21 10:57:45 +0000

    Restyled by isort

--- app/celery/count/tasks.py
+++ app/celery/count/tasks.py
@@ -1,9 +1,9 @@
 # app/tasks.py
+from app.services.count import likes, views
+
 from ...services.count.likes import add_likes
 from ...services.count.views import add_views
 from ..celery_app import celery_app
-from app.services.count import likes
-from app.services.count import views
 
 
 @celery_app.task
