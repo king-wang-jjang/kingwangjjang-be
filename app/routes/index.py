b48c833cfb2ada989485dff0f574c83d3f58e909
Restyled.io <commits@restyled.io>
2024-09-21 14:21:21 +0000

    Restyled by autopep8

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,12 +1,11 @@
 import httpx
 from fastapi import APIRouter, Request, Response, HTTPException
-from app.utils.oauth import oauth,JWT
+from app.utils.oauth import oauth, JWT
 import sys
-from app.utils.loghandler import setup_logger,catch_exception
+from app.utils.loghandler import setup_logger, catch_exception
 sys.excepthook = catch_exception
 logger = setup_logger()
 router = APIRouter()
-
 
 
 async def forward_request(request: Request, base_url: str, path: str, token: str = None) -> httpx.Response:
@@ -44,8 +43,8 @@ async def proxy(request: Request, path: str) -> Response:
             token_data = JWT().decode(token)
             logger.debug(f"JWT token issued: {token_data}")
         except HTTPException as e:
-            raise HTTPException(status_code=401, detail="Failed to verify your JWT token.")
-
+            raise HTTPException(
+                status_code=401, detail="Failed to verify your JWT token.")
 
     # 요청을 프록시 서버로 전달
     response = await forward_request(request, base_url, path, token)
