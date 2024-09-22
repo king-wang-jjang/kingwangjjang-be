a37833c8c57120dd2a4805e7f1309cddb5d94a89
Restyled.io <commits@restyled.io>
2024-09-22 12:32:11 +0000

    Restyled by black

--- app/routes/index.py
+++ app/routes/index.py
@@ -5,16 +5,21 @@ from fastapi import APIRouter, Request, Response, HTTPException
 from app.utils.oauth import oauth, JWT
 import sys
 from app.utils.loghandler import setup_logger, catch_exception
+
 sys.excepthook = catch_exception
 logger = setup_logger()
 router = APIRouter()
 
 
-async def forward_request(request: Request, base_url: str, path: str, token: str = None) -> httpx.Response:
+async def forward_request(
+    request: Request, base_url: str, path: str, token: str = None
+) -> httpx.Response:
     """프록시 서버로 요청을 전달합니다."""
     url = f"{base_url}/{path}"
     headers = dict(request.headers)
-    if not request.url.path.startswith("/callback") and not request.url.path.startswith("/login"):
+    if not request.url.path.startswith("/callback") and not request.url.path.startswith(
+        "/login"
+    ):
         headers["Authorization"] = f"Bearer {token}"
 
     async with httpx.AsyncClient() as client:
@@ -30,20 +35,22 @@ async def forward_request(request: Request, base_url: str, path: str, token: str
 
 
 @router.api_route(
-    "/{path:path}",
-    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"]
+    "/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"]
 )
 async def proxy(request: Request, path: str) -> Response:
     """프록시 요청을 처리합니다."""
     base_url = "http://localhost:8000/proxy"
     token = None
-    if not request.url.path.startswith("/callback") and not request.url.path.startswith("/login"):
+    if not request.url.path.startswith("/callback") and not request.url.path.startswith(
+        "/login"
+    ):
         try:
             token = request.headers.get("Authorization").split("Bearer ")[1]
         except HTTPException as e:
             logger.debug("Can't find token on your Requests")
             raise HTTPException(
-                status_code=401, detail="Can't find token on your Requests")
+                status_code=401, detail="Can't find token on your Requests"
+            )
         # ID 토큰 검증
         try:
             token_data = JWT().decode(token)
@@ -51,7 +58,8 @@ async def proxy(request: Request, path: str) -> Response:
         except HTTPException as e:
             logger.debug("Failed to verify your JWT token.")
             raise HTTPException(
-                status_code=401, detail="Failed to verify your JWT token.")
+                status_code=401, detail="Failed to verify your JWT token."
+            )
 
     # 요청을 프록시 서버로 전달
     response = await forward_request(request, base_url, path, token)
