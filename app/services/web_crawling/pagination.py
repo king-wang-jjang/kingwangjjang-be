e0fcfa5f1a7387dec4aa459a924f851718b8cc22
황준섭 <junseopwhang@gmail.com>
2024-10-05 13:16:29 +0900

    :bug: BoardSummaryType 관련하여 수정

--- app/services/web_crawling/pagination.py
+++ app/services/web_crawling/pagination.py
@@ -15,7 +15,7 @@ class BoardSummaryType(graphene.ObjectType):
     url = graphene.String()
     create_time = graphene.DateTime()
     GPTAnswer = graphene.String()
-    Tag = graphene.List(graphene.String)
+    Tag = graphene.List(of_type=graphene.String)
     def __init__(self, **kwargs):
         kwargs.pop('_id', None)  # '_id' 필드 제거
         super().__init__(**kwargs)
