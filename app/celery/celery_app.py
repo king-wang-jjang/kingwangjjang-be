2acee57ddd119c34a2e3e4f6bb9aa257b7946579
황준섭 <junseopwhang@gmail.com>
2024-10-21 17:09:52 +0900

    :bug: 셀러리 작동 안되는 버그 수정

--- app/celery/celery_app.py
+++ app/celery/celery_app.py
@@ -27,6 +27,9 @@ celery_app.conf.update(
         "app.celery.tasks.task_realtime_to_db": {
             "queue": "crawling_queue"
         },
+        "app.celery.tasks.auto_reboot": {
+            "queue": "crawling_queue"
+        },
         # "app.celery.tasks.task_daily_to_db": {"queue": "crawling_queue"},
     },
     result_expires=3600,
