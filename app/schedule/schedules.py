889a5ef51a088aec248b79b68c7fa6fd4bf2cc43
황준섭 <junseopwhang@gmail.com>
2024-11-16 20:22:45 +0900

    :bug: arm 아키택처 빌드 가능하게 픽스

--- app/schedule/schedules.py
+++ app/schedule/schedules.py
@@ -3,7 +3,7 @@ from app.services.web_crawling.index import get_real_time_best
 import schedule
 import time
 def threadings():
-    schedule.every(1).minute.do(get_real_time_best)
+    schedule.every(10).seconds.do(get_real_time_best)
     while True:
         schedule.run_pending()
         time.sleep(1)
