8d337e5bbf79b57e76a895f83b30719c51a6be9d
woody <woody@aicess.ai>
2024-12-12 12:02:10 +0900

    🔥 잘못 넣은 파일들 삭제

--- board-app/log/app/middlewares/static_middleware.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from app.utils.loghandler import catch_exception
-import sys
-sys.excepthook = catch_exception
-def add(app: FastAPI):
-    app.mount("/static", StaticFiles(directory="static"), name="static")
--- board-app/log/app/routes/path/ApiPaths.py
+++
@@ -1,8 +0,0 @@
-SEPARATE = "/"
-PROXY = "/proxy"
-# Graphql
-GRAPHQL = PROXY + "/graphql"
-STATUS = PROXY + "/status"
-# Restful paths
-# Auth paths
-AUTH = SEPARATE + "google"
