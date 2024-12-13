43093dab2dfd5799023f8a32f24818e7b9de7c44
woody <woody@aicess.ai>
2024-12-13 14:00:35 +0900

    🔒️ 프록시로 graphql 작업

--- api-gateway/app/routes/index.py
+++ api-gateway/app/routes/index.py
@@ -11,46 +11,34 @@ logger = setup_logger()
 
 router = APIRouter()
 
+@router.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"])
+async def proxy(request: Request, path: str) -> Response:
+    """프록시 요청을 처리합니다."""
+    
+    # prefix에 따라 포트 설정
+    if path.startswith("boardapp/"):
+        port = 8002
+        path = path[len("boardapp/"):]
+    elif path.startswith("schedulerapp/"):
+        port = 8003
+        path = path[len("schedulerapp/"):]
+    else:
+        raise HTTPException(status_code=404, detail="Invalid path prefix")
 
-async def forward_request(request: Request, base_url: str, path: str, token: str = None) -> httpx.Response:
-    """프록시 서버로 요청을 전달합니다."""
-    url = f"{base_url}/{path}"
-    headers = dict(request.headers)
-
-    if token and not request.url.path.startswith("/callback") and not request.url.path.startswith("/login"):
-        headers["Authorization"] = f"Bearer {token}"
-
+    url = f"http://localhost:{port}/{path}"
     try:
         async with httpx.AsyncClient() as client:
             response = await client.request(
                 method=request.method,
                 url=url,
-                headers=headers,
+                headers=dict(request.headers),
                 cookies=request.cookies,
                 data=await request.body(),
             )
             logger.debug(f"Forwarded request to {url} with status {response.status_code}")
-        return response
+
+            # 응답 내용을 JSON으로 변환하여 반환
+            return Response(content=response.content, status_code=response.status_code, media_type=response.headers.get('Content-Type'))
     except Exception as e:
         logger.error(f"Error forwarding request to {url}: {e}")
-        raise HTTPException(status_code=500, detail="Error forwarding request to proxy server")
-
-
-@router.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"])
-async def proxy(request: Request, path: str) -> Response:
-    """프록시 요청을 처리합니다."""
-    base_url = "http://localhost:8002/"+ path
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.request(
-                method=request.method,
-                url=base_url,
-                headers=dict(response.headers),
-                cookies=request.cookies,
-                data=await request.body(),
-            )
-            logger.debug(f"Forwarded request to {base_url} with status {response.status_code}")
-        return response
-    except Exception as e:
-        logger.error(f"Error forwarding request to {base_url}: {e}")
         raise HTTPException(status_code=500, detail="Error forwarding request to proxy server")
