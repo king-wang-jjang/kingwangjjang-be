72d0e61c972180c9a9bc291d56d9ff48f2be7264
woody <woody@aicess.ai>
2024-12-13 18:17:06 +0900

    🎨 auth service는 api-gateway에 종속

--- auth-service/main.py
+++
@@ -1,4 +0,0 @@
-# from typing import Union
-from fastapi import FastAPI
-
-app = FastAPI()
--- auth-service/prod.sh
+++
@@ -1,3 +0,0 @@
-#celery -A app.celery.celery_app worker --loglevel=debug -E &
-#celery -A app.celery.celery_app beat --loglevel=debug &
-poetry run python app/main.py
