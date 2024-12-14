5c6e7245a83b7376168290df6f98ef9bc5f259b0
hwanju <hwanju1596@gmail.com>
2024-12-15 00:22:47 +0900

    🚧 build test prod.sh

--- api-gateway/prod.sh
+++ api-gateway/prod.sh
@@ -1,3 +1,4 @@
 #celery -A app.celery.celery_app worker --loglevel=debug -E &
 #celery -A app.celery.celery_app beat --loglevel=debug &
-poetry run python app/main.py
+# poetry run python app/main.py
+poetry run uvicorn app.main:app --port 8000
