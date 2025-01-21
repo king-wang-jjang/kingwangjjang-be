0c9018b790043555ba53a0109db24e3069c7403b
woody <woody@aicess.ai>
2025-01-21 10:48:18 +0900

    💚 Port 번호 수정

--- api-gateway/prod.sh
+++ api-gateway/prod.sh
@@ -1,6 +1,6 @@
 #celery -A app.celery.celery_app worker --loglevel=debug -E &
 #celery -A app.celery.celery_app beat --loglevel=debug &
 # poetry run python app/main.py
-poetry run uvicorn app.main:app --host 0.0.0.0 --port 33331
+poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000
 
  
