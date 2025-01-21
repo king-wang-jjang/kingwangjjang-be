7952963f0ff420222a7e448ae357e55b3650697b
woody <woody@aicess.ai>
2025-01-21 17:57:42 +0900

    🔒️ cors 추가

--- api-gateway/app/middlewares/cors_middleware.py
+++ api-gateway/app/middlewares/cors_middleware.py
@@ -5,12 +5,16 @@ import sys
 from starlette.middleware.sessions import SessionMiddleware
 
 sys.excepthook = catch_exception
-# function for enabling CORS on web server
+origins = [
+    "http://localhost:8083",  
+    "https://top1.kr",  
+]
+
 def add(app: FastAPI):
     app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"])
-    app.add_middleware(SessionMiddleware, secret_key="some-random-string")
+    # app.add_middleware(SessionMiddleware, secret_key="some-random-string")
