1374bfa3fb1c9b4a28e2508f88d06dbd9d9584c8
hwanju <hwanju1596@gmail.com>
2025-01-31 19:26:44 +0900

    🚚 id -> user_id

--- api-gateway/app/graphql/queries/user_queries.py
+++ api-gateway/app/graphql/queries/user_queries.py
@@ -5,5 +5,5 @@ from app.models.auth_models import UserType
 @strawberry.type
 class UserQuery:
     @strawberry.field
-    async def get_user(self, info, id: str) -> UserType:
-        return await get_user_by_id(id)
+    async def get_user(self, info, user_id: str) -> UserType:
+        return await get_user_by_id(user_id)
