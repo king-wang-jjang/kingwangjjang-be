9dd5284da17c5ef271249dbb603d25d829309025
hwanju <hwanju1596@gmail.com>
2025-02-08 13:18:46 +0900

    🚑️ UserType에서 _id 자동생성

--- api-gateway/app/models/auth_models.py
+++ api-gateway/app/models/auth_models.py
@@ -9,7 +9,7 @@ class FilterType:
 
 @dataclass
 class UserType:
-    _id: Optional[str] = field(default=None, repr=False) 
+    _id: Optional[str] = field(init=False) 
     user_id: Optional[str] = None
     auth_organization: Optional[str] = None
     nickname: Optional[str] = None
